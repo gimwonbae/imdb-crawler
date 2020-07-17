44b2eafc5173cd24f709a36e34c4df4e3a81d90b
waristo <waristo.dev@gmail.com>
2020-07-17 15:49:36 +0900

    check csv but many error

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+import csv
+import re
 
 def nextSib(head, number):
     i = 0
@@ -31,37 +33,46 @@ class Parser:
         header = ct.find('h3', {'class': 'lister-item-header'})
       except AttributeError as e:
         header = ''
-        print(self.url, ' : HEADER ', e)  
+        # print(self.url, ' : HEADER ', e)  
       try:
         number = header.a.get('href').split('/')[2]
       except AttributeError as e:
         number = ''
-        print(self.url, ' : NUMBER ', e)
+        # print(self.url, ' : NUMBER ', e)
       try:
         title = header.a.get_text()
       except AttributeError as e:
-        print(self.url, ' : TITLE ', e)
+        title = ''
+        # print(self.url, ' : TITLE ', e)
       try:
         year = header.find('span', {'class' : 'lister-item-year'}).get_text().replace('(','').replace(')','').strip()
       except AttributeError as e:
         year = ''
-        print(self.url, ' : YEAR ', e)
+        # print(self.url, ' : YEAR ', e)
       try:  
         genre = ct.find('span', {'class' : 'genre'}).get_text().replace('\n','').strip()
       except AttributeError as e:
         genre = ''
-        print(self.url, ' : GENRE ', e)
+        # print(self.url, ' : GENRE ', e)
       try:
         star = ct.find('p', {'class' : ''}).get_text().split('\n')[-2]
       except AttributeError as e:
         star = ''
-        print(self.url, ' : STAR ', e)
+        # print(self.url, ' : STAR ', e)
       content[number] = [title, year, genre, star]
   def setNext(self) :
     return self.bs.find('a', {'class' : 'next-page'}).get('href')
     
+def makeCsv(content) :
+  csvFile = open('test.csv', 'w+', encoding='utf-8')
+  try:
+    writer = csv.writer(csvFile)
+    writer.writerow(('id','title','year','genre','star'))
+    for key, value in content.items():
+      writer.writerow((key, value[0], value[1], value[2], value[3]))
+  finally:
+    csvFile.close()
 
-  
 # class Content:
 #   def __init__(self, number, title, year, genre, star)
 #     self.number = number
@@ -83,7 +94,7 @@ while(i < maxNum) :
   parser.setContent(content)
   nextPage = home + parser.setNext()
   i+=1
-  if (i==255) :
-    print(nextPage)
-
-# print(content)
+  # if (i==255) :
+  #   print(nextPage)
+print(content['tt6256616'])
+# makeCsv(content)
